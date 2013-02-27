a91351eb94f39ce748e612f19e21051a8d07f854
Fernando Barillas <fbis251@gmail.com>
2013-02-26 16:47:45 -0800

    EFS Tools initial working backup/restore functions and buttons
    Signed-off-by: Fernando Barillas <fbis251@gmail.com>

--- src/com/fernandobarillas/SGS4GCarrierUnlocker/EfsTools.java
+++ src/com/fernandobarillas/SGS4GCarrierUnlocker/EfsTools.java
@@ -1,9 +1,43 @@
 package com.fernandobarillas.SGS4GCarrierUnlocker;
 
 public class EfsTools {
+	static Shell SHELL;
 
 	public EfsTools() {
-		// TODO 
+		SHELL = new Shell();
+		if (!SHELL.checkBusybox()) {
+			// TODO: Send some kind of warning to user here
+		}
 	}
 
+	public boolean doEfsBackup() {
+		boolean result = false;
+
+		// TODO: Expand backup to something like cm9's updater.sh to save
+		// backups that were already made.
+		SHELL.sendCommand("cd / && busybox tar cvf /sdcard/test/backup.tar /efs && cd /sdcard/test && busybox md5sum backup.tar > backup.tar.md5");
+		// System.out.println("RESULT = " + SHELL.lastExitStatus() + "\n" +
+		// SHELL);
+		if (SHELL.lastExitStatus() == 0)
+			result = true;
+
+		System.out.println("efs BACKUP result = " + result);
+		return result;
+	}
+
+	public boolean doEfsRestore() {
+		boolean result = false;
+
+		SHELL.sendCommand("cd /sdcard/test && md5sum -c backup.tar.md5 && cd / && tar xvf /sdcard/test/backup.tar");
+		if (SHELL.lastExitStatus() == 0)
+			result = true;
+		System.out.println("efs RESTORE result = " + result);
+
+		return result;
+	}
+
+	public boolean isBackupAvailable() {
+		// TODO: Check if backup exists
+		return false;
+	}
 }
--- src/com/fernandobarillas/SGS4GCarrierUnlocker/EfsToolsActivity.java
+++ src/com/fernandobarillas/SGS4GCarrierUnlocker/EfsToolsActivity.java
@@ -2,15 +2,61 @@ package com.fernandobarillas.SGS4GCarrierUnlocker;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.app.Activity;
 
 public class EfsToolsActivity extends Activity {
-	EfsTools efsTools;
+	static EfsTools EFS_TOOLS;
+	static TextView textView;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		Log.i("EfsToolsActivity", "Instantiated");
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_efs_tools);
+
+		textView = (TextView) this.findViewById(R.id.efs_tools_result_view);
+		textView.setText("");
+
+		final Button efsBackupButton = (Button) findViewById(R.id.efs_backup_button);
+		final Button efsRestoreButton = (Button) findViewById(R.id.efs_restore_button);
+		EFS_TOOLS = new EfsTools();
+
+		efsBackupButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				String text = "";
+				// TODO: Disable backup/restore buttons if busybox is not
+				// available
+
+				// We don't want users repeatedly tapping the button
+				efsBackupButton.setEnabled(false);
+				if (EFS_TOOLS.doEfsBackup()) {
+					text = "Backup successful";
+				} else {
+					text = "Backup failed";
+				}
+
+				textView.setText(text);
+				efsBackupButton.setEnabled(true);
+			}
+		});
+
+		efsRestoreButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				String text = "";
+
+				// We don't want users repeatedly tapping the button
+				efsRestoreButton.setEnabled(false);
+				if (EFS_TOOLS.doEfsRestore()) {
+					text = "Restore successful";
+				} else {
+					text = "Restore failed";
+				}
+				textView.setText(text);
+				efsRestoreButton.setEnabled(true);
+			}
+		});
 	}
 }
